e484d3ab4175b4cba19583c7866964039bc2208c
Atticus Wong <atticuswong@gmail.com>
2024-01-30 19:19:03 -0800

    change class to classNames for .tsx syntax

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,18 +1,14 @@
-import { Link } from "react-router-dom"
-
 export default function Item() {
   return (
-   
-
-      <div class="item">
-        <img class = "preview_img" src=" " alt="image goes here"></img>
-        <div class="description">
-          <div class="text">
-            <h2>Price</h2>
-            <p>Item</p>
-            <p>Location</p>
-          </div>
+    <div className="item">
+      <img className="preview_img" src=" " alt="image goes here"></img>
+      <div className="description">
+        <div className="text">
+          <h2>Price</h2>
+          <p>Item</p>
+          <p>Location</p>
         </div>
       </div>
-  )
+    </div>
+  );
 }
