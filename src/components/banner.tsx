daab848a39a8406a07e78c2d0cf7eceaf7479b94
Aiden <150979117+2Luxuria@users.noreply.github.com>
2024-02-04 22:57:03 -0800

    Add files via upload

---
+++ src/components/banner.tsx
@@ -0,0 +1,16 @@
+export default function Banner() {
+  return (
+    <div className="banner">
+      <div className="title"> 
+      <h1>Title of our Project</h1> </div>
+        <div className="slogan"> 
+        <h3>A slogan or something</h3> </div>
+          <div className="desc">
+          <p>Description #1</p> </div>
+          
+      <div className="image">
+        <img src="https://via.placeholder.com/333x429" alt="Picture goes here"/>
+      </div>
+    </div>
+  );
+}
