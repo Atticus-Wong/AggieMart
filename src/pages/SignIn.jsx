a263f8f586ead85935c65d65c76854860e69c057
Yihong Li <154848136+yihongli-04@users.noreply.github.com>
2024-05-05 15:03:49 -0700

    Google button styling Moved it below the register option.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,7 @@ import '../styles/SignIn.css';
 import NavBar from '../components/navbar.tsx';
 import signinbg from '../assets/signin-bg.jpg';
 import cutecow from '../assets/cutecow.jpg';
+import googlelogo from '../assets/google-logo.jpg';
 import {Link} from 'react-router-dom';
 import { auth, googleProvider } from '../../server/config/firebase';
 import { signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
@@ -62,7 +63,7 @@ export function LogIn() {
                 <div className="left-align">
                     <input className="email" type="email" placeholder="Email Address" onChange={(e) => {setEmail(e.target.value)} } />
                     <input className="password" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                    <button onClick={signInWithGoogle}>Sign in with google</button>
+                    {/* <button onClick={signInWithGoogle}>Sign in with google</button> */}
                     <a id="forgot-password" href="https://www.google.com">Forgot Password?</a>
                 </div>
                     <button id="submit" type="submit" onClick={signin}>Sign In</button>
@@ -71,6 +72,14 @@ export function LogIn() {
 
             <hr/>
             <p>Don't have an account? <Link to="../Register">Register</Link></p>
+            <p>OR</p>
+
+            <button id="sign-in-with-google-button" onClick={signInWithGoogle}>
+                <img src={googlelogo}></img>
+                <p>Sign in with Google</p>
+            </button>
+
+
             <img id="cutecow" src={cutecow} alt="cute cow"></img>
         </div>
     );
