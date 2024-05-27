0a1c9a45dea0922f98c566622fc150f4a985692d
Atticus Wong <atticuswong@gmail.com>
2024-05-27 00:54:51 -0700

    feat: GET queries

--- server/GET_queries.js
+++ server/GET_queries.js
@@ -1,10 +1,41 @@
 import { SqlConnect, config } from './db.js'
 import sql from 'mssql'
-function GetFirstTenItems() {
 
+async function GetXTenItems(x, y) {
+  SqlConnect();
+  var poolConnections = sql.connect(config);
+
+  const resultSet = await poolConnections.request().query(`
+    SELECT * FROM LISTINGS
+    WHERE
+    list_id BETWEEN
+    ${x} AND ${y}
+  `);
+
+  return resultSet;
+}
+
+async function nextTenItems(x, y) {
+  return GetXTenItems(x + 10, y + 10);
+}
+
+async function GetDescriptionObject(id) {
+
+  SqlConnect();
+  var poolConnections = sql.connect(config);
+
+  const resultSet = await poolConnections.request().query(`
+    SELECT * FROM DETAILS 
+    WHERE 
+    list_id = ${id}
+  `);
+
+  return resultSet;
 }
 
 export {
-  GetFirstTenItems
+  GetXTenItems,
+  GetDescriptionObject,
+  nextTenItems
 }
 
